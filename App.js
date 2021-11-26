3dbaee60f2f67694be4f076665d5236f38ffd963
SiniJohanna <82939474+SiniJohanna@users.noreply.github.com>
2021-11-26 17:51:43 +0200

    toimii, mun puhelin puhuu suomea

--- App.js
+++ App.js
@@ -1,11 +1,21 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
+import * as Speech from 'expo-speech';
 
 export default function App() {
+
+  const [text, setText] = useState('');
+  
+
+  const speak = () => {
+    Speech.speak(text);
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <TextInput style={styles.input} onChangeText={text => setText(text)} value={text} />
+      <Button title="Paina tästä kuunnellaksesi tekstin" onPress={speak} />
       <StatusBar style="auto" />
     </View>
   );
@@ -18,4 +28,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    marginTop: 50,
+    marginBottom: 5,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1 
+  }
 });
